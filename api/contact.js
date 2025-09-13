67abdb724ec0f6c1dd35da9dce12866922000a8d
info764 <info@concretecracks.ca>
2025-09-13 14:04:26 -0400

    Create contact.js

---
+++ api/contact.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
+  console.log("📩 New form submission:", req.body);
+
+  return res.status(200).json({ message: "Form received" });
+}
